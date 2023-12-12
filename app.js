96a6bed905607fb2f8348431b9b34145c870ce0c
justShashank57 <justShashank57@gmail.com>
2023-12-13 00:04:02 +0530

    Checking current user

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
-const {requireAuth} = require('./middleware/authMiddleware');
+const {requireAuth, checkUser} = require('./middleware/authMiddleware');
 const app = express();
 
 // middleware
@@ -21,6 +21,7 @@ mongoose.connect(url)
   console.log("DB connected & listening to port:3000")
 }))
 // routes
+app.get('*',checkUser);
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies',requireAuth, (req, res) => res.render('smoothies'));
 
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const userModel = require('../models/user');
 
 const requireAuth = (req,res,next) =>{
       const token = req.cookies.jwt;
@@ -19,4 +20,27 @@ const requireAuth = (req,res,next) =>{
       }
 }
 
-module.exports = {requireAuth};
+// check current users
+const checkUser = (req,res,next)=>{
+      const token = req.cookies.jwt;
+      if(token){
+         jwt.verify(token,'shashank secret',async (err,decodedToken)=>{
+             if(err){
+                console.log(err.message);
+                res.locals.user = null;
+                next();
+             }
+             else{
+                  console.log(decodedToken);
+                  let user = await userModel.findById(decodedToken.id);
+                  res.locals.user = user;
+                  next();
+             }
+         })
+      }
+      else{
+         res.locals.user =null;
+         next();
+      }
+}
+module.exports = {requireAuth, checkUser};
